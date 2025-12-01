1304f64cfcf7134736d8194e6f160218f9dc4df8
oussemayari <ayari.oussema@esprit.tn>
2025-12-01 22:43:11 +0100

    add eslint config for CI

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+    },
+  },
+];
