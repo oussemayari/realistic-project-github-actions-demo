a62d85245a3f84bab96e9de1080fb4edaf820fea
oussemayari <ayari.oussema@esprit.tn>
2025-12-01 23:00:05 +0100

    t4

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,9 @@
-export default [
-  {
-    files: ["**/*.{js,jsx}"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-    rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "error",
-    },
-  },
-];
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
+]);
